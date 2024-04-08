d21c5b4b3df16df47491ef8725a2ad23f2ce216f
bladekey88 <arun.a.sohdi+github@gmail.com>
2024-04-08 18:28:50 +0100

    Re-wrote process-user to incorporate new SQL query functions.

--- process-user.php
+++ process-user.php
@@ -1,11 +1,7 @@
 <?php
-##TODO convert to add user function
-## ALSO TODO convert to parameterised SQL/PREPARED STATEMENT
-
 require_once(__DIR__ . "/config/auth.php");
 
 //Local Functions
-
 function handle_error($error)
 {
     $_SESSION["error"] = $error;
@@ -18,17 +14,18 @@ function handle_error($error)
     exit();
 }
 
-//Use for fallback 
-$direct = ($_SERVER["CONTENT_TYPE"] == "application/x-www-form-urlencoded") ? true : false;
+#################################
+######## BEGIN EXECUTION ########
+#################################
 
 //Permissions check
 if (!check_user_permission(PERMISSION_ADD_USER)) {
     header("HTTP/1.1 403 Forbidden");
     redirect("/");
-    exit();
+    exit("Insufficient Permissions");
 }
 
-//Set up error array
+// Set up error array
 $error = array();
 
 // Check if the form is submitted
@@ -41,8 +38,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-    //Want to exit early if field values are missing
-    $error && handle_error($error);
+    // Exit early if mandatory values are missing
+    if (!empty($error)) {
+        var_dump(handle_error($error));
+    }
 
     // Retrieve user input
     $username = sanitise_user_input($_POST["username"], "username");
@@ -51,88 +50,80 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password2 = sanitise_user_input($_POST["confirm-password"]);
     $firstname = sanitise_user_input($_POST["firstname"]);
     $lastname = sanitise_user_input($_POST["lastname"]);
-    $sanitised_middlename = sanitise_user_input($_POST["middlename"]);
-    $middlename = strlen($sanitised_middlename) > 0 ? $sanitised_commonname : null;
-    $sanitised_commonname = sanitise_user_input($_POST["commonname"]);
-    $commonname = strlen($sanitised_commonname) > 0 ? $sanitised_commonname : $firstname;
+    $middlename = sanitise_user_input($_POST["middlename"]);
+    $commonname = sanitise_user_input($_POST["commonname"]);
     $house = sanitise_user_input($_POST["house"]);
     $year = sanitise_user_input($_POST["year"]);
-    $idnumber = str_pad(random_int(1000000, 1000000000), 10, "0", STR_PAD_LEFT);
-    $locked = 0;
+    $idnumber = generate_user_idnumber();
 
-    //Validations    
-    $username_pattern = "/^[a-z]\.[a-z]+[0-9]{0,2}$/";
-    //Does the user exist already with username or email?
+    // Validations 
+    // Check if username or email already exist
     if (user_exists($username, $email)) {
-        array_push($error, "The username or email address already exists");
+        array_push($error, "Username or email address already exists");
     }
-
+    // Check passwords match
     if ($password <> $password2) {
-        array_push($error, "The passwords do not match");
+        array_push($error, "Passwords do not match");
     }
-
+    // Check username format 
+    $username_pattern = "/^[a-z]\.[a-z]+[0-9]{0,2}$/";
     if (!preg_match($username_pattern, $username)) {
         array_push($error, "Username should be in the format 'a.bcd' (with numbers as necessary for distinguishing users)");
     }
-
-    if (explode("@", $email)[1] <> "hogwarts.wiz") {
-        array_push($error, "The email domain is not valid  it must be hogwarts.wiz");
+    // Check email format, then check domain if valid
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if (explode("@", $email)[1] <> "hogwarts.wiz") {
+            array_push($error, "The email domain is not valid - must be '@hogwarts.wiz'");
+        }
+    } else {
+        array_push($error, "The email address is not in a valid format");
     }
-
+    // House and Year logic checks
     if ($year == "STAFF" && $house <> "HOGWARTS") {
         array_push($error, "House must be set to Hogwarts when Year is set to Staff");
     }
-
     if ($year == "NONE" && $house <> "NONE") {
         array_push($error, "House must be set to None when Year is set to None");
     }
-
     if ($year != "NONE" && $house == "NONE") {
         array_push($error, "House must not be set to None when Year is set to $year");
     }
 
-    if ($error) {
-        $_SESSION["error"] = $error;
-        $response = [
-            'error' => $error,
-        ];
-        header("HTTP/1.1 400 Bad Request");
-        header('Content-Type: application/json');
-        echo json_encode($response);
-        exit();
-        header("Location:" . WEBROOT . "add-user.php");
-        exit();
-    }
-
-    // Good to go!
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PW, DB_DB);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    if (!empty($error)) {
+        handle_error($error);
     }
 
     // Hash the password (consider using password_hash() in production)
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-    //Year and House should be be in standard case (legacy db)
+    // Year and House should be in standard case (legacy db)
     $house = ucwords(strtolower($house));
     $year = ucwords(strtolower($year));
 
-    // Insert data into the database (replace "users" with your actual table name)
-    $sql = "INSERT INTO users (username, email, password, firstname, middlename, lastname, commonname, house, year, idnumber, locked) 
-            VALUES ('$username', '$email', '$hashed_password', '$firstname','$middlename', '$lastname', '$commonname', '$house', '$year', '$idnumber', '$locked')";
-    if ($conn->query($sql) === TRUE) {
-        echo json_encode("User added successfully");
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    // Run SQL to add user (may need to create add user function if other user creation exist)
+    $add_user = run_sql2(insert_new_user(
+        $username,
+        $email,
+        $hashed_password,
+        $firstname,
+        $lastname,
+        $middlename,
+        $commonname,
+        $house,
+        $year,
+        $idnumber
+    ));
 
-    // Close the database connection
-    $conn->close();
+    echo json_encode("User added successfully");
     redirect(WEBROOT . "/profile.php?user=$username");
-    // header("Location:" . WEBROOT . " /profile.php?user=$username");
 } else {
-    // If the form is not submitted, redirect to the add_user page
-    echo ("SOMETHING WENT WRONG");
+    // If not using a POST request
+    header('HTTP/1.1 405 Method Not Allowed');
+    header('Allow: POST');
+    header('Content-Type: application/json');
+
+    print_r(json_encode([
+        "status_code" => 405,
+        "message" => "Method not allowed"
+    ]));
+    exit;
 }
