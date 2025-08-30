89feaa6ff21731e6c9bd85104531976132947e6f
Silvertitan <65674360+bladekey88@users.noreply.github.com>
2025-08-30 02:42:15 +0100

    Updated links following restructure.

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,36 +1,36 @@
 <div class="sidebar">
-    <div class="logo" style="height: 8rem; background: url('assets/img/logo-small.png'); background-position: center; background-repeat: no-repeat; background-size: contain;">
+    <div class="logo" style="height: 8rem; background: url('/directory/assets/img/logo-small.png'); background-position: center; background-repeat: no-repeat; background-size: contain;">
         <!-- <img src="assets/img/logo-crest.png" alt="Logo - the hogwarts crest" aria-hidden="true"> -->
     </div>
     <nav class="sidebar-content" style="top: 0%; position: sticky;">
         <ul class="nav-list">
             <li class="nav-item">
                 <a class="nav-link active" href="#" aria-current="page">
-                    <img src="assets/img/home.svg" aria-hidden="true" alt="Go to Home Page">
+                    <img src="/directory/assets/img/home.svg" aria-hidden="true" alt="Go to Home Page">
                     <span>Home</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="index2.php">
-                    <img src="assets/img/user.svg" aria-hidden="true" alt="Go to Users Page">
+                    <img src="/directory/assets/img/user.svg" aria-hidden="true" alt="Go to Users Page">
                     <span>Users</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="assets/img/vle.svg" aria-hidden="true" alt="Go to VLE Info Page">
+                    <img src="/directory/assets/img/vle.svg" aria-hidden="true" alt="Go to VLE Info Page">
                     <span>VLE</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="assets/img/email.svg" aria-hidden="true" alt="Go to Email Info Page">
+                    <img src="/directory/assets/img/email.svg" aria-hidden="true" alt="Go to Email Info Page">
                     <span>Email</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="statute.php">
-                    <img src="assets/img/Statute.svg" aria-hidden="true" alt="Go to Statute Page">
+                    <img src="/directory/assets/img/Statute.svg" aria-hidden="true" alt="Go to Statute Page">
                     <span>Statute</span>
                 </a>
             </li>
