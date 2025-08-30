bc03f5e45cad29400233b23759d74c64a58c1db0
Silvertitan <65674360+bladekey88@users.noreply.github.com>
2025-08-30 02:41:47 +0100

    Moved Certificate Login Info under nav header for presentation purposes

--- components/header.php
+++ components/header.php
@@ -25,17 +25,7 @@ if (!isset($title)) {
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;9..40,500;9..40,700&display=swap">
     <link href="https://fonts.googleapis.com/css2?family=Murecho:wght@100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/directory/assets/css/style.css">
 </head>
 
 <body>
-    <header>
-        <?php if ($_SESSION["login_method"] == "CLIENT_CERTIFICATE") : ?>
-            <div class="alert" role="status" style="text-align: center; vertical-align: middle;">
-                <span>
-                    A valid client certificate was presented to this site for this account, enabling automatic login.
-                    You can also disable certificate login for this account from your <a class="link-offset-1" href="<?php echo WEBROOT; ?>/profile.php#certificates">profile page.</a>
-                </span>
-            </div>
-        <?php endif; ?>
-    </header>
--- components/nav-header.php
+++ components/nav-header.php
@@ -9,10 +9,20 @@
         <li aria-label="Year">Y: <?php echo $_SESSION["year"]; ?></li>
         <li aria-label="Role">R: <?php echo $_SESSION["role"]; ?></li>
         <li aria-label="View My Profile">
-            <a href="<?php echo WEBROOT; ?>/profile.php"><?php echo $_SESSION["name"]; ?></a>
+            <a class="user-nav-link" href="<?php echo WEBROOT; ?>/profile.php"><?php echo $_SESSION["name"]; ?></a>
         </li>
         <li aria-label="Logout">
-            <a href="<?php echo WEBROOT; ?>/logout.php">Logout</a>
+            <a class="user-nav-link" href="<?php echo WEBROOT; ?>/logout.php">Logout</a>
         </li>
     </ul>
 </nav>
+<header>
+    <?php if ($_SESSION["login_method"] == "CLIENT_CERTIFICATE") : ?>
+        <div class="alert alert-narrower" role="status" style="text-align: center; vertical-align: middle;">
+            <span>
+                A valid client certificate was presented to this site for this account, enabling automatic login.
+                You can also disable certificate login for this account from your <a class="link-offset-1" href="<?php echo WEBROOT; ?>/profile.php#certificates">profile page.</a>
+            </span>
+        </div>
+    <?php endif; ?>
+</header>
